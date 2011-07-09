3ca60ee53ea7fb5cef9975ca33ed7cbea8258f07
Michael Mulich <michael.mulich@gmail.com>
2011-07-09 12:52:01 -0400

    Encapsulation of the project crawl process.

--- index2repo/cli.py
+++ index2repo/cli.py
@@ -2,7 +2,8 @@
 """
 Create a Python metadata repository from a Python Package Index.
 
-This is a MESS, because it attempts to clean up an even bigger mess.
+The implementation is a MESS, because it attempts to clean up
+an even bigger mess.
 """
 import os
 import logging
@@ -13,31 +14,14 @@ from packaging.pypi.simple import Crawler, DEFAULT_SIMPLE_INDEX_URL
 
 from pkgmeta.index2repo.config import LOGGER_NAME
 
+logger = logging.getLogger(LOGGER_NAME)
 
-def main():
-    parser = argparse.ArgumentParser(description="Index to Repository")
-    parser.add_argument('-d', '--debug', action='store_true', default=False)
-    parser.add_argument('--all-releases',
-                        action='store_true', default=False,
-                        help="Retrieve all releases not just the "\
-                             "designated stable release. (default: False)")
-    args = parser.parse_args()
-    logger = logging.getLogger(LOGGER_NAME)
 
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
+def _random_project_sample(projects):
+    """ XXX For development testing purposes only...
 
-    package_downloads = os.path.join(os.curdir, 'downloads')
-    package_downloads = os.path.abspath(package_downloads)
-    if not os.path.exists(package_downloads):
-        os.mkdir(package_downloads)
-
-    
-    crawler = Crawler(follow_externals=False)
-    projects = crawler.search_projects('')
-
-    # XXX Randomly selection a sample from ~16000 packages with 4% error
-    #     acceptance, so about 974 packages in the sample size
+    Randomly select a sample from ~16000 packages with 4% error
+    acceptance, so about 974 packages in the sample size."""
     _sample_size = 974
     count = 0
     new_projects_list = []
@@ -59,24 +43,22 @@ def main():
             logger.info("  Selecting: {0}".format(p.name))
             new_projects_list.append(p)
             count += 1
-    projects = new_projects_list
-    # XXX end sample size block
+    return new_projects_list
 
-    collected_metadata = []
+def _releases_generator_function(crawler, projects, all_releases=False):
+    """Yield the project and a list of releases for that project.
+
+    By default, the release list will contain only one value unless
+    the all_releases argument is set to True."""
     for project in projects:
         package_name = project.name
-        package_dir = os.path.join(package_downloads, package_name)
-        if not os.path.exists(package_dir):
-            os.mkdir(package_dir)
-        logger.debug("Downloading {0} to {1}".format(package_name,
-                                                     package_dir))
 
         try:
             # XXX Must call get_releases because get_release doesn't support
             #     the force_update argument.
             #     
             releases = crawler.get_releases(package_name, force_update=True)
-            if not args.all_releases and len(releases) > 0:
+            if not all_releases and len(releases) > 0:
                 release = crawler.get_release(package_name,
                                               prefer_final=True)
                 releases = [release]
@@ -89,8 +71,44 @@ def main():
                          .format(package_name, err))
             shutil.rmtree(package_dir)
             continue
+        yield project, releases
 
-        for release_info in releases:
+def crawl_index(all_releases=False):
+    """Crawl the index for project releases."""
+    crawler = Crawler(follow_externals=False)
+    projects = crawler.search_projects('')
+    projects = _random_project_sample(projects)
+    return _releases_generator_function(crawler, projects, all_releases)
+
+def main():
+    parser = argparse.ArgumentParser(description="Index to Repository")
+    parser.add_argument('-d', '--debug', action='store_true', default=False)
+    parser.add_argument('--all-releases',
+                        action='store_true', default=False,
+                        help="Retrieve all releases not just the "\
+                             "designated stable release. (default: False)")
+    args = parser.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    package_downloads = os.path.join(os.curdir, 'downloads')
+    package_downloads = os.path.abspath(package_downloads)
+    if not os.path.exists(package_downloads):
+        os.mkdir(package_downloads)
+
+    crawl = crawl_index(args.all_releases)
+
+    collected_metadata = []
+    for project, release_set in crawl:
+        package_name = project.name
+        package_dir = os.path.join(package_downloads, package_name)
+        if not os.path.exists(package_dir):
+            os.mkdir(package_dir)
+        logger.debug("Downloading {0} to {1}".format(package_name,
+                                                     package_dir))
+
+        for release_info in release_set:
             version = str(release_info.version)
             download_dir = os.path.join(package_dir, version)
             if os.path.exists(download_dir):
