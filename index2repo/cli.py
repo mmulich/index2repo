c75c568c93f2541adc57420c9be33f77cd06a435
Michael Mulich <michael.mulich@gmail.com>
2011-07-10 12:05:09 -0400

    Logger object removed from the module space. This effects how we handle exceptions in the project release fetch process. _releases_generator_function returns a single object now. It was returning the same object with a different before.

--- index2repo/cli.py
+++ index2repo/cli.py
@@ -14,71 +14,77 @@ from packaging.pypi.simple import Crawler, DEFAULT_SIMPLE_INDEX_URL
 
 from pkgmeta.index2repo.config import LOGGER_NAME
 
-logger = logging.getLogger(LOGGER_NAME)
+XXX_sample_size = None
 
+
+# ################# #
+#   Functionality   #
+# ################# #
 
 def _random_project_sample(projects):
     """ XXX For development testing purposes only...
 
     Randomly select a sample from ~16000 packages with 4% error
     acceptance, so about 974 packages in the sample size."""
-    _sample_size = 974
+    global XXX_sample_size
+    _sample_size = XXX_sample_size or 974
     count = 0
     new_projects_list = []
     from random import choice
-    logger.info("Randomly sampling:")
     # XXX special cases that should be tests =/
+    # Insert special case: redsolutioncms.django-chunks IrrationalVersionError
     # Insert special case: Yarrow 1.2 Download IO error
     # Insert special case: sake 0.0 Download URL error
-    # special_cases = ('sake', 'Yarrow')
+    # special_cases = ('sake', 'Yarrow',)
     # for p in projects:
     #     if p.name in special_cases:
-    #         logger.info("Selecting special case: {0}".format(p.name))
     #         count += 1
     #         new_projects_list.append(p)
     # end special cases
     while count != _sample_size:
         p = choice(projects)
         if p not in new_projects_list:
-            logger.info("  Selecting: {0}".format(p.name))
             new_projects_list.append(p)
             count += 1
     return new_projects_list
 
-def _releases_generator_function(crawler, projects, all_releases=False):
+def _releases_generator_function(projects, prefer_final=True):
     """Yield the project and a list of releases for that project.
 
     By default, the release list will contain only one value unless
-    the all_releases argument is set to True."""
-    for project in projects:
-        package_name = project.name
-
+    the prefer_final argument is set to False."""
+    while projects:
+        project = projects.pop()
         try:
-            # XXX Must call get_releases because get_release doesn't support
-            #     the force_update argument.
-            #     
-            releases = crawler.get_releases(package_name, force_update=True)
-            if not all_releases and len(releases) > 0:
-                release = crawler.get_release(package_name,
-                                              prefer_final=True)
-                releases = [release]
-        except IrrationalVersionError as err:
-            logger.error("  Skipping {0} due to irrational version: {1}"\
-                         .format(package_name, err))
-            continue
+            # XXX There is an issue with the Crawler API that forces
+            #     us to use the index rather than the resulting project.
+            #     See also http://bugs.python.org/issue12526
+            project = project._index.get_releases(project.name,
+                                                  prefer_final=prefer_final,
+                                                  force_update=True)
         except Exception as err:
-            logger.error("  Removing {0} due to an unknown error: {1}"\
-                         .format(package_name, err))
-            shutil.rmtree(package_dir)
-            continue
-        yield project, releases
+            # FIXME I don't like this, but lack a better solution at
+            #       the moment.
+            setattr(project, 'failure', err)
+        yield project
 
 def crawl_index(all_releases=False):
     """Crawl the index for project releases."""
     crawler = Crawler(follow_externals=False)
     projects = crawler.search_projects('')
     projects = _random_project_sample(projects)
-    return _releases_generator_function(crawler, projects, all_releases)
+    return _releases_generator_function(projects, not all_releases)
+
+def download_project_releases(project, release_set,
+                              download_directory="downloads"):
+    """Download the the project's releases."""
+    # TODO What kind of release should we download? source or bin?
+    pass
+
+
+# ################ #
+#   Main program   #
+# ################ #
 
 def main():
     parser = argparse.ArgumentParser(description="Index to Repository")
@@ -87,8 +93,16 @@ def main():
                         action='store_true', default=False,
                         help="Retrieve all releases not just the "\
                              "designated stable release. (default: False)")
+    # XXX Used in manual testing...
+    parser.add_argument('--xxx-sample-size', type=int, nargs=1)
     args = parser.parse_args()
 
+    # XXX values
+    if args.xxx_sample_size:
+        global XXX_sample_size
+        XXX_sample_size = args.xxx_sample_size[0]
+
+    logger = logging.getLogger(LOGGER_NAME)
     if args.debug:
         logger.setLevel(logging.DEBUG)
 
@@ -100,15 +114,19 @@ def main():
     crawl = crawl_index(args.all_releases)
 
     collected_metadata = []
-    for project, release_set in crawl:
+    for project in crawl:
         package_name = project.name
+        if len(project) == 0:
+            logger.info("Skipping {0} because it has no releases."\
+                        .format(package_name))
+            continue
         package_dir = os.path.join(package_downloads, package_name)
         if not os.path.exists(package_dir):
             os.mkdir(package_dir)
         logger.debug("Downloading {0} to {1}".format(package_name,
                                                      package_dir))
 
-        for release_info in release_set:
+        for release_info in project:
             version = str(release_info.version)
             download_dir = os.path.join(package_dir, version)
             if os.path.exists(download_dir):
@@ -122,7 +140,7 @@ def main():
             #       downloaded from... and infact I need this information
             #       for the X-PkgMeta-Download value.
             logger.info("  Downloading {0} ({1})...".format(package_name,
-                                                       version))
+                                                            version))
             try:
                 download_path = release_info.download(download_dir)
             except IrrationalVersionError as err:
